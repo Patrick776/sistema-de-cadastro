74f6542855d5d7391d7a45eb5dac32769736d911
Patrick776 <75255193+Patrick776@users.noreply.github.com>
2023-10-30 11:37:27 -0300

    corrigindo o app js

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("aplicação está se conectando ao banco de dados")
 console.log("Patrick realizou commit sem presença da aula")
+console.log("Enviando para o GitHub")
